a04c12bf09327741087a0e1faaa3defe7372b17d
cjwoodru2 <cjwoodru2@gmail.com>
2017-01-12 22:49:09 -0500

    Test again for heroku fix

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3000
 app.use(express.static("public"));
 
 app.use(function (req, res, next) {
-	if (req.headers['x-forwarded-pronto'] === 'https') {
-		res.redirect('http://'+req.hostname + req.url);
+	if (req.headers['x-forwarded-proto'] === 'https') {
+		res.redirect('http://'+ req.hostname + req.url);
 	} else {
 		next()
 	}
